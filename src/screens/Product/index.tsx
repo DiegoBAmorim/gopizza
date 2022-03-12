29786570cecb2633ac6d28bddd19d20ec6d002e6
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2022-03-12 16:42:00 -0300

    Complete Product screen

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,24 +1,162 @@
-import React from 'react';
-import {Platform, TouchableOpacity} from 'react-native';
+import React, { useState } from "react";
+import { Platform, TouchableOpacity, ScrollView, Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
 
-import {ButtonBack} from '../../components/ButtonBack';
-import {Photo} from '../../components/Photo';
+import { ButtonBack } from "../../components/ButtonBack";
+import { Photo } from "../../components/Photo";
+import { InputPrice } from "../../components/InputPrice";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
-import {Container, Header, Title, DeleteLabel} from './styles';
+import {
+  Container,
+  Header,
+  Title,
+  DeleteLabel,
+  Upload,
+  PickImageButton,
+  Form,
+  Label,
+  InputGroup,
+  InputGroupHeader,
+  MaxCharacters,
+} from "./styles";
 
 export function Product() {
-    return(
-        <Container
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-        >
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [priceSizeP, setPriceSizeP] = useState("");
+  const [priceSizeM, setPriceSizeM] = useState("");
+  const [priceSizeG, setPriceSizeG] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status === "granted") {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4],
+      });
+
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    }
+  }
+
+  async function handleAdd() {
+    if (!image) {
+      return Alert.alert("Cadastro", "Selecione a imagem da pizza");
+    }
+
+    if (!name.trim()) {
+      return Alert.alert("Cadastro", "Informe o nome da pizza");
+    }
+    if (!description.trim()) {
+      return Alert.alert("Cadastro", "Informe a descrição da pizza");
+    }
+    if (!priceSizeP || !priceSizeG || !priceSizeG) {
+      return Alert.alert(
+        "Cadastro",
+        "Informe o preço de todos os tamanhos da pizza"
+      );
+    }
+
+    setIsLoading(true);
+
+    const fileName = new Date().getTime();
+    const reference = storage().ref(`/pizzas/${fileName}.png`);
+
+    await reference.putFile(image);
+    const photo_url = await reference.getDownloadURL();
+
+    firestore()
+      .collection("pizzas")
+      .add({
+        name,
+        name_insensitive: name.toLowerCase().trim(),
+        description,
+        prices_sizes: {
+          p: priceSizeP,
+          m: priceSizeM,
+          g: priceSizeG,
+        },
+        photo_url,
+        photo_path: reference.fullPath,
+      })
+      .then(() => Alert.alert("Cadastro", "Pizza cadastrada com sucesso"))
+      .catch(() =>
+        Alert.alert("Cadastro", "Não foi possível cadastrar a pizza")
+      );
+
+    setIsLoading(false);
+  }
+  return (
+    <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <Header>
-            <Title>Cadastrar</Title>
-            <TouchableOpacity>
-                <ButtonBack onPress={() => {}}/>
-                <DeleteLabel>Deletar</DeleteLabel>
-            </TouchableOpacity>
+          <ButtonBack onPress={() => {}} />
+          <Title>Cadastrar</Title>
+          <TouchableOpacity>
+            <DeleteLabel>Deletar</DeleteLabel>
+          </TouchableOpacity>
         </Header>
-        <Photo uri="https://github.com/rodrigorgtic.png" />
-        </Container>
-    )
+        <Upload>
+          <Photo uri={image} />
+          <PickImageButton
+            title="carregar"
+            type="secondary"
+            onPress={handlePickerImage}
+          />
+        </Upload>
+        <Form>
+          <InputGroup>
+            <Label>Nome</Label>
+            <Input onChangeText={setName} value={name} />
+          </InputGroup>
+          <InputGroup>
+            <InputGroupHeader>
+              <Label>Descrição</Label>
+              <MaxCharacters>0 de 60 caracteres</MaxCharacters>
+            </InputGroupHeader>
+            <Input
+              multiline
+              maxLength={60}
+              style={{ height: 80 }}
+              onChangeText={setDescription}
+              value={description}
+            />
+          </InputGroup>
+          <InputGroup>
+            <Label>Tamanhos e Preços</Label>
+            <InputPrice
+              size="P"
+              onChangeText={setPriceSizeP}
+              value={priceSizeP}
+            />
+            <InputPrice
+              size="M"
+              onChangeText={setPriceSizeM}
+              value={priceSizeM}
+            />
+            <InputPrice
+              size="G"
+              onChangeText={setPriceSizeG}
+              value={priceSizeG}
+            />
+          </InputGroup>
+
+          <Button
+            title="cadastrar pizza"
+            isLoading={isLoading}
+            onPress={handleAdd}
+          />
+        </Form>
+      </ScrollView>
+    </Container>
+  );
 }
