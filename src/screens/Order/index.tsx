9dfc60e94e69f4d8299f57e87c9a3cc702aaae74
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2022-03-22 09:26:12 -0300

    Creating radioButton and fix layout

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Platform } from "react-native";
 
-import { ButtonBack } from "../../components/ButtonBack";
+import { PIZZA_TYPES } from "../../utils/pizzaTypes";
 
-import { Container, Header } from "./styles";
+import { ButtonBack } from "../../components/ButtonBack";
+import { RadioButton } from "../../components/RadioButton";
+
+import { Container, Header, Photo, Sizes } from "./styles";
 
 export function Order() {
+  const [size, setSize] = useState("");
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Header>
         <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
       </Header>
+
+      <Photo source={{ uri: "https://github.com/diegobamorim.png" }} />
+      <Sizes>
+        {PIZZA_TYPES.map((item) => (
+          <RadioButton
+            key={item.id}
+            title={item.name}
+            onPress={() => setSize(item.id)}
+            selected={size === item.id}
+          />
+        ))}
+      </Sizes>
     </Container>
   );
 }
--- src/screens/Order/styles.ts
+++ src/screens/Order/styles.ts
@@ -12,3 +12,21 @@ export const Header = styled(LinearGradient).attrs(({theme}) => ({
 }))`
 padding: ${getStatusBarHeight() + 34}px 42px 0px;
 `
+
+export const Photo = styled.Image`
+width: 240px;
+height: 240px;
+border-radius: 120px;
+align-self: center;
+position: relative;
+top: -120px;
+
+`;
+
+export const Sizes = styled.View`
+width: 100%;
+flex-direction: row;
+justify-content: space-between;
+margin-bottom: 40px;
+
+`;
